0d47419ada98344defdfe7936ab33a0005258c57
alvaroiporre <alvaroiporremartinez@gmail.com>
2023-06-28 13:27:18 -0400

    Fix linters

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,8 +1,8 @@
 import React, { useEffect, useRef, useState } from 'react';
 import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 import shapes from '../assets/country_shapes.json';
 import { getLimits, formData } from '../maps/maps';
-import { Link } from 'react-router-dom';
 
 const Map = () => {
   const selectedCountry = useSelector((state) => state.countries.selectedCountry) || 'BO';
